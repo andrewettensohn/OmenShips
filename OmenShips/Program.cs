3fdc42cbe30ed381aaf12bdd83922e0d6cfc10e0
andrewettensohn <andrewettensohn@gmail.com>
2021-12-29 21:45:25 -0500

    Add API routes in config

--- OmenShips/Program.cs
+++ OmenShips/Program.cs
@@ -15,7 +15,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new Settings { BaseApiRoute = builder.Configuration["OmenTestAPIRoute"] });
+
+string apiBaseRoute = builder.HostEnvironment.IsDevelopment() ? builder.Configuration["OmenTestAPILocalRoute"] : builder.Configuration["OmenTestAPIProdRoute"];
+
+builder.Services.AddScoped(sp => new Settings { BaseApiRoute = apiBaseRoute });
 builder.Services.AddScoped<IOmenTestRestService, OmenTestRestService>();
 
 builder.Services.AddMudServices(config =>
